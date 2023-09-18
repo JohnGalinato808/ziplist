29999d5470b6fe1a7cd0c6f28aa6ec6da043fa8a
JohnGalinato808 <johnathan.galinato808@gmail.com>
2023-09-17 14:36:25 -1000

    E26
    ziplist Practrice WOD

---
+++ ziplist.js
@@ -0,0 +1,19 @@
+const _ = require('underscore');
+
+const testListOne = ['a', 'b', 'c'];
+const testListTwo = [1, 2, 3];
+function zipList(listOne, listTwo) {
+  const combinedList = [];
+  for (let i = 0; i < listOne.length; i++) {
+    combinedList.push(listOne[i], listTwo[i]);
+  }
+  return combinedList;
+}
+
+console.log(zipList(testListOne, testListTwo));
+
+function zipListTheSimpleWay(listOne, listTwo) {
+  return _.flatten(_.zip(listOne, listTwo));
+}
+
+console.log(zipListTheSimpleWay(testListOne, testListTwo));
